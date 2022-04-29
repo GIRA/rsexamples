25a09517e55c9cb2cdbf432240a0292b1fcc0687
RichoM <richi.moran@gmail.com>
2022-04-29 11:04:41 -0300

    Added comments

--- python/robot.py
+++ python/robot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import math_utils.angle as Angle
 from math_utils.point import Point
 from math_utils.utils import clamp
@@ -7,20 +8,33 @@ from collections import deque
 
 MAX_SPEED = 10
 
-class Robot:    
-    leftVelocity = 0
-    rightVelocity = 0
-    teamMessages = deque()
+# Esta clase representa un robot. Incluye funciones primitivas de navegación
+# que permiten mover al robot en la cancha. Estas funciones de navegación no 
+# tienen un efecto inmediato en el simulador sino que recién se aplican en el 
+# siguiente ciclo de simulación.
+class Robot:
+    role = None # El rol define el comportamiento del robot
+    snapshot = None # La snapshot contiene la información actualizada del mundo
+    leftVelocity = 0 # Velocidad del motor izquierdo
+    rightVelocity = 0 # Velocidad del motor derecho
+    teamMessages = deque() # Mensajes a enviar al resto del equipo
 
     def __init__(self, role):
         self.role = role
 
+    # La posición del robot la obtenemos de la snapshot actual
     def getPosition(self):
         return self.snapshot.robot.position
 
+    # La rotación del robot la obtenemos de la snapshot actual
     def getRotation(self):
         return self.snapshot.robot.rotation
 
+    # La función "loop" se ejecuta en cada iteración. Recibe la información
+    # del mundo y devuelve el mensaje a enviar al simulador, el cual contiene
+    # las velocidades a aplicar a cada motor y los mensajes para el resto
+    # del equipo. 
+    # El comportamiento del robot se define en el método "run" (ver más abajo)
     def loop(self, data):
         self.snapshot = Snapshot(data)
         self.run()
@@ -32,9 +46,12 @@ class Robot:
             "R": self.rightVelocity
         }
 
+    # Encola un mensaje para el resto del equipo
     def sendDataToTeam(self, data):
         self.teamMessages.append(data)
 
+    # Modifica la velocidad de los motores de forma que el robot gire hacia 
+    # el ángulo especificado. Tiene en cuenta la simetría del robot.
     def lookAtAngle(self, a):
         ra = self.getRotation()
         delta = min(Angle.diff(a, ra), Angle.diff(a, Angle.opposite(ra)))
@@ -59,6 +76,8 @@ class Robot:
         self.leftVelocity = vl * MAX_SPEED
         self.rightVelocity = vr * MAX_SPEED
 
+    # Modifica la velocidad de los motores de forma que el robot gire para
+    # "mirar" al punto especificado. Tiene en cuenta la simetría del robot
     def lookAtPoint(self, point):
         position = self.getPosition()
         rx = position.x
@@ -67,6 +86,8 @@ class Robot:
         py = point.y
         self.lookAtAngle(Point(px - rx, py - ry).getAngle())
 
+    # Modifica la velocidad de los motores de manera que el robot se acerque
+    # al punto especificado. Tiene en cuenta la simetría del robot.
     def moveToPoint(self, point):
         position = self.getPosition()
         rx = position.x
@@ -91,12 +112,19 @@ class Robot:
         self.leftVelocity = vl * MAX_SPEED
         self.rightVelocity = vr * MAX_SPEED
 
+    # Modifica la velocidad de los motores de forma que el robot se acerque a 
+    # la pelota. Tiene en cuenta la simetría del robot.
     def moveToBall(self):
         self.moveToPoint(self.snapshot.ball.position)
 
+    # El método "run" implementa la lógica de comportamiento del robot
     def run(self):
+        # Si el robot detectó la señal de la pelota en este ciclo de simulación 
+        # le comunica esta información a sus compañeros de forma que todos los
+        # robots tengan información aproximada de la ubicación de la pelota
         if self.snapshot.isBallDetected():
             pos = self.snapshot.ball.position
             self.sendDataToTeam([pos.x, pos.y])
 
+        # El comportamiento del robot depende del rol que tenga asignado
         self.role.applyOn(self, self.snapshot)
--- python/snapshot.py
+++ python/snapshot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from dataclasses import dataclass
 from math_utils.angle import radians
 from math_utils.point import Point
@@ -14,12 +15,15 @@ class RobotData:
 class BallData:
     position: Point
 
-
+# Esta clase representa la información que tenemos del ambiente de simulación
+# en el ciclo de ejecución actual.
+# Incluye los datos provenientes de los sensores del robot y aplica las transformaciones 
+# necesarias para obtener posición/rotación del robot y posición de la pelota. 
 class Snapshot:
-    data = None
-    color = None
-    robot = None
-    ball = None
+    data = None # Los datos provenientes del simulador
+    color = None # Color del equipo (Y o B)
+    robot = None # Datos del robot (posición y rotación)
+    ball = None # Datos de la pelota (posición)
 
     def __init__(self, data):
         self.data = data
@@ -28,9 +32,14 @@ class Snapshot:
         self.processBallSignal(data.get("ball"))
         self.mergeTeamData(data.get("team"))
 
+    # Devuelve true si el robot detectó la señal de la pelota. Esto nos permite
+    # distinguir si la información que tenemos de la pelota proviene de un compañero
+    # o si es propia.
     def isBallDetected(self):
         return self.data.get("ball") != None
 
+    # Procesa los sensores del robot (gps y compass) para obtener la posición y
+    # rotación del robot
     def processRobotSensors(self, robot_data):
         x, y, _ = robot_data["gps"]
         cx, cy, _ = robot_data["compass"]
@@ -40,6 +49,10 @@ class Snapshot:
             position=Point(x, y),
             rotation=radians(math.atan2(cx, cy) + math.pi/2))
 
+    # Procesa la señal de la pelota (dirección e intensidad) para obtener la
+    # posición de la misma. El cálculo requiere primero obtener la info del
+    # robot porque la dirección e intensidad de la señal son relativas a la
+    # posición y orientación del robot.
     def processBallSignal(self, ball_data):
         if ball_data == None: return None
         dist = math.sqrt(1/ball_data["strength"])
@@ -52,7 +65,10 @@ class Snapshot:
         by = self.robot.position.y + dy
         self.ball = BallData(position=Point(bx, by))
 
+    # Incorporamos la información enviada por el resto del equipo (si la hubiera)
     def mergeTeamData(self, team_data):
+        # Si ya tenemos información de la pelota significa que la detectamos de
+        # primera mano y podemos ignorar los mensajes del equipo
         if self.ball == None:
             if team_data and len(team_data) > 0:
                 x, y = team_data[0]
